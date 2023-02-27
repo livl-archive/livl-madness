1da39a6b08fb676867e79bd506904e750f564239
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-02-27 22:08:42 +0100

    refactor: lobby menu and add some style to it

--- Assets/Scripts/CanvasHUD.cs
+++ Assets/Scripts/CanvasHUD.cs
@@ -6,17 +6,18 @@ using UnityEngine.UI;
 
 public class CanvasHUD : MonoBehaviour
 {
-    public GameObject PanelStart;
-    public GameObject PanelStop;
+    [SerializeField]
+	public GameObject PanelStart;
     
-    public Button buttonHost, buttonServer, buttonClient, buttonStop;
+	[SerializeField]
+    public Button buttonHost, buttonServer, buttonClient;
     
+	[SerializeField]
     public InputField inputFieldAddress;
-    
-    public Text serverText;
-    public Text clientText;
-
 	
+	[SerializeField]
+	public Text textInfo; 
+   
     private void Start()
     {
         //Update the canvas text if you have manually changed network managers address from the game object before starting the game scene
@@ -29,7 +30,6 @@ public class CanvasHUD : MonoBehaviour
         buttonHost.onClick.AddListener(ButtonHost);
         buttonServer.onClick.AddListener(ButtonServer);
         buttonClient.onClick.AddListener(ButtonClient);
-        buttonStop.onClick.AddListener(ButtonStop);
 
         //This updates the Unity canvas, we have to manually call it every change, unlike legacy OnGUI.
         SetupCanvas();
@@ -43,75 +43,31 @@ public class CanvasHUD : MonoBehaviour
 
     public void ButtonHost()
     {
-        NetworkManager.singleton.StartHost();
+		textInfo.text = "Please wait. Connecting...";
+		NetworkManager.singleton.StartHost();
         SetupCanvas();
     }
 
     public void ButtonServer()
     {
-        NetworkManager.singleton.StartServer();
+        textInfo.text = "Please wait. Connecting...";
+		NetworkManager.singleton.StartServer();
         SetupCanvas();
     }
 
     public void ButtonClient()
-    {
-        NetworkManager.singleton.StartClient();
-        SetupCanvas();
-    }
-
-    public void ButtonStop()
-    {
-        // stop host if host mode
-        if (NetworkServer.active && NetworkClient.isConnected)
-        {
-            NetworkManager.singleton.StopHost();
-        }
-        // stop client if client-only
-        else if (NetworkClient.isConnected)
-        {
-            NetworkManager.singleton.StopClient();
-        }
-        // stop server if server-only
-        else if (NetworkServer.active)
-        {
-            NetworkManager.singleton.StopServer();
-        }
-
+    {   
+		textInfo.text = "Please wait. Connecting...";
+		NetworkManager.singleton.StartClient();
         SetupCanvas();
     }
 
     public void SetupCanvas()
     {
         // Here we will dump majority of the canvas UI that may be changed.
-
-        if (!NetworkClient.isConnected && !NetworkServer.active)
+		if (NetworkClient.isConnected && NetworkServer.active)
         {
-            if (NetworkClient.active)
-            {
-                PanelStart.SetActive(false);
-                PanelStop.SetActive(true);
-                clientText.text = "Connecting to " + NetworkManager.singleton.networkAddress + "..";
-            }
-            else
-            {
-                PanelStart.SetActive(true);
-                PanelStop.SetActive(false);
-            }
-        }
-        else
-        {
-            PanelStart.SetActive(false);
-            PanelStop.SetActive(true);
-
-            // server / client status message
-            if (NetworkServer.active)
-            {
-                serverText.text = "Server: active. Transport: " + Transport.active;
-            }
-            if (NetworkClient.isConnected)
-            {
-                clientText.text = "Client: address=" + NetworkManager.singleton.networkAddress;
-            }
+			PanelStart.SetActive(false);
         }
     }
 }
