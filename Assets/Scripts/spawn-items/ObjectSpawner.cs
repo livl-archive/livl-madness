d089fc52f0b5195396985182adcc347583b92558
unknown <rdipietro@cfai-formation.fr>
2023-03-02 14:54:51 +0100

    feat: create script to spawn item on shelf and add spawnpoints

--- Assets/Scripts/spawn-items/ObjectSpawner.cs
+++ Assets/Scripts/spawn-items/ObjectSpawner.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject[] items;
+    public List<GameObject> items;
+    public List<GameObject> spawnPoints;
+
+    private List<int> spawnIdxOccupied = new List<int>();
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            int rndIdx = Random.Range(0, items.Length);
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 0, Random.Range(-10, 11));
+            int rndItemIdx = Random.Range(0, items.Count);
 
-            Instantiate(items[rndIdx], randomSpawnPosition, Quaternion.identity);
+            GameObject rndSpawnPoint = spawnPoints[getRandomSpawnPointIdx()];
+            Vector3 randomSpawnPosition = new Vector3(rndSpawnPoint.transform.position.x, rndSpawnPoint.transform.position.y, rndSpawnPoint.transform.position.z);
+
+            Instantiate(items[rndItemIdx], randomSpawnPosition, Quaternion.identity);
         }
+    }
+
+    int getRandomSpawnPointIdx()
+    {
+        int rndIdx = Random.Range(0, spawnPoints.Count);
+        while(spawnIdxOccupied.Contains(rndIdx) && spawnIdxOccupied.Count < spawnPoints.Count) 
+        {
+            rndIdx = Random.Range(0, spawnPoints.Count);
+        }
+        spawnIdxOccupied.Add(rndIdx);
+        return rndIdx;
     }
 }
